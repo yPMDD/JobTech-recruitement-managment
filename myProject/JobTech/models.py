4b94503f329566efad1d0f82781575fe50581c2d
yPMDD <saad989011@gmail.com>
2025-05-30 00:48:40 +0100

    + fixing bugs

--- myProject/JobTech/models.py
+++ myProject/JobTech/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 from users.models import CustomUser
-# from .models import Job
 
 
-# class Skill(models.Model):
-#     job_id = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='skills')
-#     name = models.CharField(max_length=100, unique=True)
-    
-#     def __str__(self):
-#         return self.name
+
 
 class Job(models.Model):
     title = models.CharField(
